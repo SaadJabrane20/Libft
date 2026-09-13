bb01963d7302b9bfeb6d1ed4db24f43625447aff
SaadJabrane20 <sjabrane20@gmail.com>
2026-09-14 00:36:52 +0100

    added str functions, and checking functions like isalpha, isdigit etc

--- includes/libft.h
+++ includes/libft.h
@@ -6,13 +6,24 @@
 /*   By: sajabran <sajabran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/12 22:54:47 by sajabran          #+#    #+#             */
-/*   Updated: 2026/09/12 22:54:47 by sajabran         ###   ########.fr       */
+/*   Updated: 2026/09/14 00:19:43 by sajabran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 #include <stddef.h>
-void *memcpy(void *dest, const void *src, size_t n);
+#include <stdlib.h>
 
+void	*memcpy(void *dest, const void *src, size_t n);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+int		ft_strncmp(char *s1, char *s2, unsigned int n);
 #endif
