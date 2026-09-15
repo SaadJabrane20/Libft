d941a7a68aebf99de98f311b5a9b49a143d7137f
SaadJabrane20 <sjabrane20@gmail.com>
2026-09-15 18:49:28 +0100

    added memset, memmove, bzero and strnstr

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,11 +6,11 @@
 /*   By: sajabran <sajabran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/14 18:15:32 by sajabran          #+#    #+#             */
-/*   Updated: 2026/09/14 18:22:53 by sajabran         ###   ########.fr       */
+/*   Updated: 2026/09/15 17:36:26 by sajabran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "includes/libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -21,9 +21,24 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	i = 0;
 	s = (unsigned char *)src;
 	d = (unsigned char *)dest;
-	while (i < n)
+	if (d == s || n == 0)
+		return (dest);
+	if (d > s && d < s + n)
 	{
-		d[i] = s[i];
-		i++;
+		i = n;
+		while (i > 0)
+		{
+			d[i - 1] = s[i - 1];
+			i--;
+		}
 	}
+	else
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
 }
