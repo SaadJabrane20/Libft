70155904111e2075db72e0d7f5871b4935e70ebe
SaadJabrane20 <sjabrane20@gmail.com>
2026-09-15 18:52:29 +0100

    fixed norminette for ft_memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: sajabran <sajabran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/14 18:15:32 by sajabran          #+#    #+#             */
-/*   Updated: 2026/09/15 17:36:26 by sajabran         ###   ########.fr       */
+/*   Updated: 2026/09/15 18:51:59 by sajabran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,23 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*d;
 	unsigned char	*s;
 
-	i = 0;
-	s = (unsigned char *)src;
 	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
 	if (d == s || n == 0)
 		return (dest);
-	if (d > s && d < s + n)
-	{
-		i = n;
-		while (i > 0)
-		{
-			d[i - 1] = s[i - 1];
-			i--;
-		}
-	}
-	else
+	i = 0;
+	if (d < s)
 	{
 		while (i < n)
 		{
 			d[i] = s[i];
 			i++;
 		}
+	}
+	else
+	{
+		while (n-- > 0)
+			d[n] = s[n];
 	}
 	return (dest);
 }
