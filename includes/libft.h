9245e09890baa05082b042b24c711d671dae3c60
SaadJabrane20 <sjabrane20@gmail.com>
2026-09-20 16:34:00 +0000

    added Linked lists functions such as : add_back, add_front, clear, delone, iter, last, map, new and size

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: sajabran <sajabran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/12 22:54:47 by sajabran          #+#    #+#             */
-/*   Updated: 2026/09/20 17:18:59 by sajabran         ###   ########.fr       */
+/*   Updated: 2026/09/20 17:33:47 by sajabran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void			ft_lstadd_back(t_list **alst, t_list *new);
 void			ft_lstclear(t_list **lst, void (*del)(void*));
 void			ft_lstdelone(t_list *lst, void (*del)(void*));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 #endif
